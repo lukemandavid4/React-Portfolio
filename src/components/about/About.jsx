8fcd919eb63ce2e95762c0e2760dd692f4169f39
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-08 16:44:35 +0300

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,37 @@
+import './about.css'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='about'>
+      <h4>Get to Know</h4>
+      <h1>About Me</h1>
+      <div className="about__center">
+        <div className="about__center--left">
+          <img src="../../src/assets/portfolio.jpg" />
+        </div>
+        <div className="about__center--right">
+          <div className="right__top">
+            <div>
+              <h6>Experience</h6>
+              <p>3+ Years Working</p>
+            </div>
+            <div>
+              <h6>Clients</h6>
+              <p>50+ Coutrywide</p>
+            </div>
+            <div>
+              <h6>Projects</h6>
+              <p>50+ Completed</p>
+            </div>
+          </div>
+          <div className="right__middle">
+            <span>An experience software developer adept at bringing forth expertise in the design, installation, testing, and maintenance of software systems. Proficient in various platforms, languages and embedded systems. Experienced with the latest cutting-edge development tools and procedures. Able to effectively self-manage during independent projects, as well as collaborative as part of a productive team.</span>
+          </div>
+          <div className="right__bottom">
+            <a href="#">Let&apos;s Talk</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,18 @@
+import { GrHomeRounded } from "react-icons/gr";
+import { IoPersonOutline } from "react-icons/io5";
+import { RiBook2Line } from "react-icons/ri";
+import { FaLaptopCode } from "react-icons/fa";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="navbar">
+      <GrHomeRounded />
+      <IoPersonOutline />
+      <RiBook2Line />
+      <FaLaptopCode />
+      <BiMessageSquareDetail />
     </div>
   )
 }
