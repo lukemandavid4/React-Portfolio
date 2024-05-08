a6f5b78a326b2aaa4ce4080c1e105b6f19665f9e
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-08 14:31:22 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import About from "./components/about/About"
+import Contact from "./components/contact/Contact"
+import Experiences from "./components/experiences/Experiences"
+import Footer from "./components/footer/Footer"
+import Home from "./components/home/Home"
+import Navbar from "./components/navbar/Navbar"
+import Projects from "./components/projects/Projects"
+import Testimonials from "./components/testimonials/Testimonials"
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Home />
+      <Navbar />
+      <About />
+      <Experiences />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,19 @@
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className="home">
+      <div className="home__header">
+        <h4>Hello I&apos;m</h4>
+        <h1>David Mwangi</h1>
+        <p>Full-Stack Developer</p>
+      </div>
+      <div className="home__cv">
+        <a href="" download>Download CV</a>
+        <a href="#">Let&apos;s Talk</a>
+      </div>
+      <div className="home__image">
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
