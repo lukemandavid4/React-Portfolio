2fdb5d858f73c9912114baeecb841551f1f454df
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-18 13:03:56 +0300

    updated

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,40 @@
+import './testimonials.css'
 
 const Testimonials = () => {
   return (
-    <div>
-      
+    <div className='testimonials'>
+      <h4>Review from clients</h4>
+      <h1>Testimonials</h1>
+      <div className="testimonials__center">
+        <div className='testimonials__center--top'>
+          <div className='testimonial__one testimonial'>
+            <div></div>
+            <h5>Lauren Smith</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem esse corporis aliquid cumque explicabo provident officiis harum tempore. Sed, ab! Hic iste officia dolor quibusdam ratione quia reprehenderit recusandae dolorem sit, cumque harum totam corrupti tempore laudantium doloribus autem ullam rem at est nihil modi ipsa. Aut nulla ullam amet.</p>
+          </div>
+          <div className='testimonial__two testimonial'>
+            <div></div>
+            <h5>Lauren Smith</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem esse corporis aliquid cumque explicabo provident officiis harum tempore. Sed, ab! Hic iste officia dolor quibusdam ratione quia reprehenderit recusandae dolorem sit, cumque harum totam corrupti tempore laudantium doloribus autem ullam rem at est nihil modi ipsa. Aut nulla ullam amet.</p>
+          </div>
+          <div className='testimonial__three testimonial'>
+            <div></div>
+            <h5>Lauren Smith</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem esse corporis aliquid cumque explicabo provident officiis harum tempore. Sed, ab! Hic iste officia dolor quibusdam ratione quia reprehenderit recusandae dolorem sit, cumque harum totam corrupti tempore laudantium doloribus autem ullam rem at est nihil modi ipsa. Aut nulla ullam amet.</p>
+          </div>
+          <div className='testimonial__four testimonial'>
+            <div></div>
+            <h5>Lauren Smith</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem esse corporis aliquid cumque explicabo provident officiis harum tempore. Sed, ab! Hic iste officia dolor quibusdam ratione quia reprehenderit recusandae dolorem sit, cumque harum totam corrupti tempore laudantium doloribus autem ullam rem at est nihil modi ipsa. Aut nulla ullam amet.</p>
+          </div>
+        </div>
+        <div className="testimonials__center--bottom">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   )
 }
