12a6b938da8b49d41e2301caa7b56ac464d76b51
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-18 19:15:14 +0300

    updated

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,12 @@
+import {  Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import './testimonials.css'
 import AVTR1 from '../../assets/avatar1.jpg'
 import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
-import { Pagination } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 const Testimonials = () => {
   const data = [
@@ -35,28 +35,25 @@ const Testimonials = () => {
     <div className='testimonials'>
       <h4>Review from clients</h4>
       <h1>Testimonials</h1>
-      <div className="testimonials__center">
-        <Swiper className='testimonials__center--top'>
-          {data.map(({avatar, name, review}, index) =>{
-            return(
-              <SwiperSlide className='testimonial__one testimonial' key={index}>
-                <div>
-                  <img src={avatar} />
-                </div>
-                <h5>{name}</h5>
-                <p>{review}</p>
-            </SwiperSlide>
-            )
-          })}  
-        </Swiper>
-        <div className="testimonials__center--bottom">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+      <Swiper className='testimonials__center'
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      >
+        {data.map(({avatar, name, review}, index) =>{
+          return(
+            <SwiperSlide className='testimonial__center--one' key={index}>
+              <div>
+                <img src={avatar} />
+              </div>
+              <h5>{name}</h5>
+              <p>{review}</p>
+          </SwiperSlide>
+          )
+        })}  
+      </Swiper>
       </div>
-    </div>
   )
 }
 
