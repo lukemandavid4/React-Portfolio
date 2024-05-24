030eec1e1eb1fa68746d5f25b855520d9e681eec
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-24 17:14:09 +0300

    updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,25 @@
+import './projects.css'
 
 const Projects = () => {
   return (
     <div className="projects">
-      <div className="projects__top"></div>
+      <h4>My Recent Work</h4>
+      <h1>Projects</h1>
+      <div className="projects__card">
+        <div className="projects__card--background"></div>
+        <div className="projects__card--name"></div>
+        <div className="projects__card--links"></div>
+      </div>
+      <div className="projects__card">
+        <div className="projects__card--background"></div>
+        <div className="projects__card--name"></div>
+        <div className="projects__card--links"></div>
+      </div>
+      <div className="projects__card">
+        <div className="projects__card--background"></div>
+        <div className="projects__card--name"></div>
+        <div className="projects__card--links"></div>
+      </div>
     </div>
   )
 }
