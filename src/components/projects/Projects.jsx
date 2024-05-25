52c0a5501e704561a86cc05933c30b4108e640d4
lukemandavid4 <lukemandavid4@gmail.com>
2024-05-25 23:19:13 +0300

    updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,23 +2,38 @@ import './projects.css'
 
 const Projects = () => {
   return (
-    <div className="projects">
+    <div className="projects" id='projects'>
       <h4>My Recent Work</h4>
       <h1>Projects</h1>
-      <div className="projects__card">
-        <div className="projects__card--background"></div>
-        <div className="projects__card--name"></div>
-        <div className="projects__card--links"></div>
-      </div>
-      <div className="projects__card">
-        <div className="projects__card--background"></div>
-        <div className="projects__card--name"></div>
-        <div className="projects__card--links"></div>
-      </div>
-      <div className="projects__card">
-        <div className="projects__card--background"></div>
-        <div className="projects__card--name"></div>
-        <div className="projects__card--links"></div>
+      <div className="projects__cards">
+        <div className="projects__card one">
+          <div className="projects__card--background"></div>
+          <div className="projects__card--name">
+            <span>Food Ordering App that allows customers to order different types of food</span>
+          </div>
+          <div className="projects__card--links">
+            <a href="#" id='github'>Github</a>
+            <a href="#" id='live'>Live Demo</a>
+          </div>
+        </div>
+        <div className="projects__card two">
+          <div className="projects__card--background"></div>
+          <div className="projects__card--name">
+            <span>EnergyUp Industries with the companies details</span>
+          </div>
+          <div className="projects__card--links">
+            <a href="#" id='github'>Github</a>
+            <a href="#" id='live'>Live Demo</a>
+          </div>
+        </div>
+        <div className="projects__card three">
+          <div className="projects__card--background"></div>
+          <div className="projects__card--name"></div>
+          <div className="projects__card--links">
+            <a href="#" id='github'>Github</a>
+            <a href="#" id='live'>Live Demo</a>
+          </div>
+        </div>
       </div>
     </div>
   )
